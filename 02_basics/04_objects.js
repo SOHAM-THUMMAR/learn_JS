31bdd8f6d80f07a24f55e9809465aec2fbae5ba8
SOHAM-THUMMAR <sohamthummar04@gmail.com>
2026-01-01 18:52:43 +0530

    learned  about destructuring and  some json

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -66,26 +66,125 @@ const Users = [
 
 Users[1].email;
 
-console.log(tinderuser1);
+// console.log(tinderuser1);
 
-console.log(Object.keys(tinderuser1));
-//this will give the list of all the keys in "tindweruser1"
-console.log(Object.values(tinderuser1));
-//this will give the list of all the values in "tinderuser1"
-console.log(Object.entries(tinderuser1))
-// this will give all the key-value pair in lists
+// console.log(Object.keys(tinderuser1));
+// //this will give the list of all the keys in "tindweruser1"
+// console.log(Object.values(tinderuser1));
+// //this will give the list of all the values in "tinderuser1"
+// console.log(Object.entries(tinderuser1))
+// //this will give all the key-value pair in lists
 
 
-console.log(tinderuser1.hasOwnProperty('isLoggedIn'));
-console.log(tinderuser1.hasOwnProperty('isLogged'));
+// console.log(tinderuser1.hasOwnProperty('isLoggedIn'));
+// console.log(tinderuser1.hasOwnProperty('isLogged'));
 // for chaking if that propertiy exist
 // this will return boolean values
 // and if we dont take care of this then sometimes projects gets crashed
 // which is not in production work
 
 
+// DESTRUCTURING ---------------------------------------------------------------------
+// (objects)
+let course = {
+  courseName : "java script",
+  price:"10000",
+  courseInsructor : "aarzoo sir"
+}
+
+// console.log(course.courseInsructor);
+// if we have to use this data multiple time and it is in deep nested object 
+// then we get too long address which is too confusing and sometimes we get error because of some little mistakes 
+// but we can avoide that by extracting that data from the object like given below
+
+let {courseInsructor} = course;
+// console.log(courseInsructor);
+// we dont need to write long long address of the data 
+let {courseInsructor: faculty} = course;
+// console.log(faculty);
+
+// JSON API ---------------------------------------------------------------------------
 
 
+// (json api type 1)
+// ================================================
+// {
+//   "name" : "coco",
+//   "email": "coco@gmail.com",
+//   "work" : "study",
+// }
+// ================================================
+// (json api type 2)
+// ================================================
+// {
+//   "results": [
+//     {
+//       "gender": "female",
+//       "name": {
+//         "title": "Miss",
+//         "first": "Jennie",
+//         "last": "Nichols"
+//       },
+//       "location": {
+//         "street": {
+//           "number": 8929,
+//           "name": "Valwood Pkwy",
+//         },
+//         "city": "Billings",
+//         "state": "Michigan",
+//         "country": "United States",
+//         "postcode": "63104",
+//         "coordinates": {
+//           "latitude": "-69.8246",
+//           "longitude": "134.8719"
+//         },
+//         "timezone": {
+//           "offset": "+9:30",
+//           "description": "Adelaide, Darwin"
+//         }
+//       },
+//       "email": "jennie.nichols@example.com",
+//       "login": {
+//         "uuid": "7a0eed16-9430-4d68-901f-c0d4c1c3bf00",
+//         "username": "yellowpeacock117",
+//         "password": "addison",
+//         "salt": "sld1yGtd",
+//         "md5": "ab54ac4c0be9480ae8fa5e9e2a5196a3",
+//         "sha1": "edcf2ce613cbdea349133c52dc2f3b83168dc51b",
+//         "sha256": "48df5229235ada28389b91e60a935e4f9b73eb4bdb855ef9258a1751f10bdc5d"
+//       },
+//       "dob": {
+//         "date": "1992-03-08T15:13:16.688Z",
+//         "age": 30
+//       },
+//       "registered": {
+//         "date": "2007-07-09T05:51:59.390Z",
+//         "age": 14
+//       },
+//       "phone": "(272) 790-0888",
+//       "cell": "(489) 330-2385",
+//       "id": {
+//         "name": "SSN",
+//         "value": "405-88-3636"
+//       },
+//       "picture": {
+//         "large": "https://randomuser.me/api/portraits/men/75.jpg",
+//         "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
+//         "thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+//       },
+//       "nat": "US"
+//     }
+//   ],
+//   "info": {
+//     "seed": "56d27f4a53bd5441",
+//     "results": 1,
+//     "page": 1,
+//     "version": "1.4"
+//   }
+// }
+
+// tools to understand the apis =============================
+// https://jsonformatter.org/
 
 
 

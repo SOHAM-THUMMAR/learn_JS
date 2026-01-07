6cc3b7510db05dd93e004bf70b9fb31a68f15ae2
SOHAM-THUMMAR <sohamthummar04@gmail.com>
2026-01-08 00:14:07 +0530

    filter done

--- 05_iterations/filter.js
+++ 05_iterations/filter.js
@@ -38,7 +38,33 @@ myNums.forEach((num) => {
 })
 console.log(nums);
 
+// example of filers
 
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+  // where genre is history
+const userBook = books.filter( (book) => book.genre ==="History");
+console.log(userBook);
+
+console.log()
+// where published year >= 2000
+const userBookV2 = books.filter( (book) => { return book.publish >= 2000});
+console.log(userBookV2);
+
+console.log()
+// where book is published after 1995 and genre is history
+const userBookV3 = books.filter( (book) => { return book.publish >= 1995 && book.genre === "History"});
+console.log(userBookV3);
 
 
 
